f76f2d3276d7cdc16d89d2c890c673cfe57eaf8a
zhangwei <zhangwei@dpandora.cn>
2019-03-13 10:28:05 +0800

    改为https

--- commonlibrary/src/main/java/com/mcmo/z/commonlibrary/constants/NetSiteCons.java
+++ commonlibrary/src/main/java/com/mcmo/z/commonlibrary/constants/NetSiteCons.java
@@ -1,5 +1,5 @@
 package com.mcmo.z.commonlibrary.constants;
 
 public class NetSiteCons {
-    public static final String BASE_URL="http://www.wanandroid.com";
+    public static final String BASE_URL="https://www.wanandroid.com";
 }
